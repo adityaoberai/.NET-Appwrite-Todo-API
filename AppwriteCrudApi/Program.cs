540ffc6db55c3d95b20561e5802bfe80fd28c158
adityaoberai <adityaoberai1@gmail.com>
2023-07-17 15:54:47 +0530

    Swagger UI updates

--- AppwriteCrudApi/Program.cs
+++ AppwriteCrudApi/Program.cs
@@ -1,19 +1,23 @@
 using Appwrite;
 using Appwrite.Services;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Built with Appwrite and .NET", Version = "v1" });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
 
 app.UseHttpsRedirection();
 
